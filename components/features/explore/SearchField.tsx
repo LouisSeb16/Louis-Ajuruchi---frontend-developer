182425bb3e606caaa47f100d5786378c0aef0893
LouisSeb16 <sebastineokey@gmail.com>
2024-01-01 10:16:34 +0100

    Fixes done

--- components/features/explore/SearchField.tsx
+++ components/features/explore/SearchField.tsx
@@ -3,47 +3,52 @@ import style from "@/styles/explore/index.module.scss";
 
 const SearchField = (props: any) => {
   const {
-    store: { searchRockets, filteredRockets, categories },
+    store: { searchRockets, filteredRockets, categories, rockets },
     action: { setRocketSearch },
   } = props;
+
   return (
     <div className={`container-fluid ${style.search}`}>
       <div className="container" id={style.con}>
         <h1>Rockets</h1>
-        <div className={style.form}>
-          <input
-            type="text"
-            placeholder="Search rocket"
-            value={searchRockets}
-            onChange={(e) => setRocketSearch(e.target.value)}
-          />
-          <div>
-            <select
-              name=""
-              id=""
+        {rockets.length > 0 && (
+          <div className={style.form}>
+            <input
+              type="text"
+              placeholder="Search rocket"
+              value={searchRockets}
               onChange={(e) => setRocketSearch(e.target.value)}
-            >
-              <option value="">Type</option>
-              {categories && categories.type?.map((data: any, index: any) => (
-                <option value={data} key={index}>
-                  {data}
-                </option>
-              ))}
-            </select>
-            <select
-              name=""
-              id=""
-              onChange={(e) => setRocketSearch(e.target.value)}
-            >
-              <option value="">Country</option>
-              {categories && categories.country?.map((data: any, index: any) => (
-                <option value={data} key={index}>
-                  {data}
-                </option>
-              ))}
-            </select>
+            />
+            <div>
+              <select
+                name=""
+                id=""
+                onChange={(e) => setRocketSearch(e.target.value)}
+              >
+                <option value="">Type</option>
+                {categories &&
+                  categories.type?.map((data: any, index: any) => (
+                    <option value={data} key={index}>
+                      {data}
+                    </option>
+                  ))}
+              </select>
+              <select
+                name=""
+                id=""
+                onChange={(e) => setRocketSearch(e.target.value)}
+              >
+                <option value="">Country</option>
+                {categories &&
+                  categories.country?.map((data: any, index: any) => (
+                    <option value={data} key={index}>
+                      {data}
+                    </option>
+                  ))}
+              </select>
+            </div>
           </div>
-        </div>
+        )}
         {searchRockets.length > 0 && (
           <p>
             {filteredRockets.length} matching result(s) for {searchRockets}
